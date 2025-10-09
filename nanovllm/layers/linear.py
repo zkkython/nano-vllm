fdeb595fed27146a259eed657095e464502fb920
zkkython <zkkython@gmail.com>
2025-10-09 17:18:10 +0800

    add comments for qkv weight loader

--- nanovllm/layers/linear.py
+++ nanovllm/layers/linear.py
@@ -122,16 +122,20 @@ class QKVParallelLinear(ColumnParallelLinear):
         total_num_kv_heads: int | None = None,
         bias: bool = False,
     ):
-        self.head_size = head_size
-        self.total_num_heads = total_num_heads
-        self.total_num_kv_heads = total_num_kv_heads or total_num_heads
+        self.head_size = head_size  # 128
+        self.total_num_heads = total_num_heads  # 16
+        self.total_num_kv_heads = total_num_kv_heads or total_num_heads  # 8
         tp_size = dist.get_world_size()
-        self.num_heads = divide(self.total_num_heads, tp_size)
-        self.num_kv_heads = divide(self.total_num_kv_heads, tp_size)
-        input_size = hidden_size
+        self.num_heads = divide(
+            self.total_num_heads, tp_size
+        )  # 单个tp rank的head数量，如果tp_size=8, 则是16/8=2
+        self.num_kv_heads = divide(
+            self.total_num_kv_heads, tp_size
+        )  # 单个tp rank的kv head数量，如果tp_size=8, 则是8/8=1
+        input_size = hidden_size  # 1024
         output_size = (
             self.total_num_heads + 2 * self.total_num_kv_heads
-        ) * self.head_size
+        ) * self.head_size  # （16 + 2 * 8） * 128 = 4096, 相当于q,k,v三个矩阵的拼接结果
         super().__init__(input_size, output_size, bias)
 
     def weight_loader(
@@ -140,18 +144,22 @@ class QKVParallelLinear(ColumnParallelLinear):
         param_data = param.data
         assert loaded_shard_id in ["q", "k", "v"]
         if loaded_shard_id == "q":
-            shard_size = self.num_heads * self.head_size
+            shard_size = self.num_heads * self.head_size  # 256
             shard_offset = 0
         elif loaded_shard_id == "k":
-            shard_size = self.num_kv_heads * self.head_size
+            shard_size = self.num_kv_heads * self.head_size  # 128
             shard_offset = self.num_heads * self.head_size
         else:
-            shard_size = self.num_kv_heads * self.head_size
+            shard_size = self.num_kv_heads * self.head_size  # 128
             shard_offset = (
                 self.num_heads * self.head_size + self.num_kv_heads * self.head_size
             )
-        param_data = param_data.narrow(self.tp_dim, shard_offset, shard_size)
-        loaded_weight = loaded_weight.chunk(self.tp_size, self.tp_dim)[self.tp_rank]
+        param_data = param_data.narrow(
+            self.tp_dim, shard_offset, shard_size
+        )  # 取出当前tp_rank对应的权重数据(q，k,v 合并在一起，通过不同行来区分)
+        loaded_weight = loaded_weight.chunk(self.tp_size, self.tp_dim)[
+            self.tp_rank
+        ]  # 从模型文件中获取tp_rank 对应的权重数据
         param_data.copy_(loaded_weight)
 
 
