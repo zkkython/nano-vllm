8411181c8a46184c03a1db1ec147930255174ab4
zkkython <zkkython@gmail.com>
2025-10-12 00:01:54 +0800

    support qwen2

--- examples/model_structure.py
+++ examples/model_structure.py
@@ -1,8 +1,16 @@
-from transformers import Qwen3ForCausalLM
+from transformers import Qwen3ForCausalLM, Qwen2ForCausalLM
 
 model_name = "/home/kason/.cache/modelscope/hub/models/Qwen/Qwen3-0.6B"
 model = Qwen3ForCausalLM.from_pretrained(
-    model_name, torch_dtype="auto", device_map="auto"
+    model_name, dtype="auto", device_map="auto"
+)
+print(f"model config = \n {model.config}")
+print("=" * 20)
+print(model)
+
+qwen2_06b = "/home/kason/models/qwen206b"
+model = Qwen2ForCausalLM.from_pretrained(
+    qwen2_06b, dtype="auto", device_map="auto"
 )
 print(f"model config = \n {model.config}")
 print("=" * 20)
---
+++ qwen2_infer.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+from nanovllm import LLM, SamplingParams
+from transformers import AutoTokenizer
+
+
+def main(args):
+    path = os.path.expanduser(args.model)
+    tokenizer = AutoTokenizer.from_pretrained(path)
+    llm = LLM(path, enforce_eager=True, tensor_parallel_size=1)
+
+    sampling_params = SamplingParams(temperature=0.6, max_tokens=256)
+    prompts = [
+        "introduce yourself",
+        "list all prime numbers within 100",
+    ]
+    prompts = [
+        tokenizer.apply_chat_template(
+            [{"role": "user", "content": prompt}],
+            tokenize=False,
+            add_generation_prompt=True,
+            enable_thinking=True,
+        )
+        for prompt in prompts
+    ]
+    outputs = llm.generate(prompts, sampling_params)
+
+    for prompt, output in zip(prompts, outputs):
+        print("\n")
+        print(f"Prompt: {prompt!r}")
+        print(f"Completion: {output['text']!r}")
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--model", type=str, default="/home/kason/models/qwen206b")
+    args = args.parse_args()
+    main(args)
